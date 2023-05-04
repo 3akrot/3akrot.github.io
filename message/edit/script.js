226a28c779bb517d81dc8d2859a1edf540e5e27a
3akrot <105954803+3akrot@users.noreply.github.com>
2023-05-04 22:27:00 +0300

    سشسسش
    سش

--- message/edit/script.js
+++ message/edit/script.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('textInput');
 const output = document.getElementById('outputSpan');
 const saveButton = document.getElementById('saveButton');
+const maxChars = 300;
 
 // Load the saved text from localStorage on page load
 output.textContent = localStorage.getItem('savedText') || '';
@@ -8,6 +9,17 @@ output.textContent = localStorage.getItem('savedText') || '';
 input.addEventListener('input', (event) => {
   // Update the text in the span element
   output.textContent = event.target.value;
+  
+  // Check the length of the input value
+  const charsLeft = maxChars - event.target.value.length;
+  const charsLeftSpan = document.getElementById('charsLeftSpan');
+  
+  // Update the characters left message
+  if (charsLeft >= 0) {
+    charsLeftSpan.textContent = charsLeft + ' فاضل';
+  } else {
+    charsLeftSpan.textContent = 'Maximum character limit reached';
+  }
 });
 
 saveButton.addEventListener('click', () => {
@@ -15,8 +27,12 @@ saveButton.addEventListener('click', () => {
   if (input.value.trim() !== '') {
     // Save the text to localStorage
     localStorage.setItem('savedText', input.value);
-    alert('Changes saved!');
+    alert('تم الحفظ');
   } else {
     alert('حنزر الحقل فاضي هتشغل دماغك عليا ولا اي');
   }
 });
+
+
+
+
