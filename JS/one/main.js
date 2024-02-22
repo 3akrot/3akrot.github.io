daa141ba254d10714ffc8fd4c108f9107ee8dc9f
3akrot <105954803+3akrot@users.noreply.github.com>
2024-02-22 21:14:01 +0200

    new

--- JS/one/main.js
+++ JS/one/main.js
@@ -1,14 +1,32 @@
 let create = document.querySelector("input[type=submit]");
-
+let myselection = document.querySelector("select");
+let vidid = document.getElementById("vid");
+window.onload = function () {
+  let eltypr = document.querySelector("select[name=type]");
+  eltypr.value = "Div"
+}
+myselection.oninput = function () {
+  if (myselection.value === "iframe") {
+    vidid.classList.remove("hidden");
+  } else {
+    vidid.classList.add("hidden");
+  }
+};
 create.onclick = function (e) {
-  document.getElementsByClassName("results")[0].innerHTML =""
+  let url = `https://www.youtube.com/embed/${vidid.value}`;
+  document.getElementsByClassName("results")[0].innerHTML = "";
   e.preventDefault();
   let elnums = document.querySelector("input[type=number]");
   let eltypr = document.querySelector("select[name=type]");
   let eltex = document.querySelector("input[name=texts]");
   for (let i = 0; i < elnums.value; i++) {
     let newel = document.createElement(`${eltypr.value}`);
-    newel.innerHTML = eltex.value
+    if (eltypr.value === "iframe") {
+      newel.setAttribute("src",url)
+    }
+    else{
+    newel.innerHTML = eltex.value;
+  }
     document.getElementsByClassName("results")[0].appendChild(newel);
   }
 };
