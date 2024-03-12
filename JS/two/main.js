7115c96e4351b08a15e1c3e014fb43b563c632d4
3akrot <105954803+3akrot@users.noreply.github.com>
2024-03-13 00:06:16 +0200

    new version

--- JS/two/main.js
+++ JS/two/main.js
@@ -1,68 +1,40 @@
 let inputfield = document.querySelector("input[type = text]");
 let button = document.querySelector(".form button");
-let i;
-let x = document.querySelector(".x");
+let tasks = document.querySelector(".tasks");
 
-let arr;
-setInterval(function(){
-  
-  arr = document.querySelectorAll(".tasks .task");
-  updatelocal()
-},100)
-function updatelocal(){
-  for (let i = 0; i < arr.length; i++) {
-    localStorage.setItem(`task${i}`,arr[i].getAttribute("data-cont"));    
-  }
-}
-
-
-
-
-
-
-button.onclick = function () {
+document.querySelector(".container").addEventListener("click", function (e) {
   let val = inputfield.value;
-  sessionStorage.setItem("val",inputfield.value);
-  let b = document.createElement("div");
-  b.classList.add("task");
-  b.textContent = val;
-  let del = document.createElement("button");
-  del.innerHTML = "remove";
-  del.classList.add("del");
-  del.onclick = function () {
-    del.parentElement.style.cssText = "animation: del 0.3s forwards;";
-    setTimeout(function () {
-      del.parentElement.remove();
-      localStorage.clear();
-    }, 300);
-  };
-  b.appendChild(del);
-  b.setAttribute("data-cont",val);
-  b.style.cssText = "animation: add 0.5s forwards;";
-  document.querySelector(".tasks").appendChild(b);
-  // b.style.cssText = "animation:"
-};
-//load elments
-window.onload = function () {
-  inputfield.value = sessionStorage.getItem("val")
-  for (let i = 0; i < localStorage.length; i++) {
+
+  sessionStorage.setItem("val", inputfield.value);
+  if (val == "") {
+  } else if (e.target === button) {
     let b = document.createElement("div");
-    b.classList.add("task");
-    b.textContent = localStorage.getItem(`task${i}`);
-    let del = document.createElement("button");
+    b.textContent = val;
+    let del = document.createElement("span");
     del.innerHTML = "remove";
     del.classList.add("del");
-    del.onclick = function () {
-      del.parentElement.style.cssText = "animation: del 0.3s forwards;";
-      setTimeout(function () {
-        del.parentElement.remove();
-        localStorage.clear();
-      }, 300);
-    };
     b.appendChild(del);
-  b.setAttribute("data-cont",localStorage.getItem(`task${i}`));
-  // b.style.cssText = "animation: add 0.5s forwards;";
-  document.querySelector(".tasks").appendChild(b);
+    b.style.cssText = "animation: add 0.5s forwards;";
+    document.querySelector(".tasks").appendChild(b);
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.style.cssText = "animation: del 0.3s forwards;";
+    setTimeout(function () {
+      e.target.parentElement.remove();
+      sav();
+    }, 300);
+  } else if (e.target.tagName === "DIV") {
+    e.target.classList.toggle("checked");
   }
-};
+  console.log(e.target);
+  sav();
+});
 
+//load elments
+
+function sav() {
+  localStorage.setItem("data", tasks.innerHTML);
+}
+function load() {
+  tasks.innerHTML = localStorage.getItem("data");
+}
+load();
