80b4e0b5096f684a9c8e2573f16ebd2c9fc72c51
3akrot <105954803+3akrot@users.noreply.github.com>
2024-04-12 22:23:31 +0200

    Update main.js

--- JS/two/main.js
+++ JS/two/main.js
@@ -66,28 +66,29 @@ document.querySelector(".container").addEventListener("click", function (e) {
   } else if (e.target.tagName === "SPAN") {
     document.getElementById("sound-del").load();
     document.getElementById("sound-del").play();
-    e.target.parentElement.style.cssText = "animation: del 5s forwards;";
+    e.target.parentElement.style.cssText = "animation: del 0.5s forwards;";
     e.target.parentElement.classList.add("ondel");
     // if(e.target.parentElement.parentElement.nextSibling !== null){
 
-      if (isfull() == false && e.target.parentElement.parentElement.nextSibling === null){ 
-        e.target.parentElement.parentElement.style.cssText =
-        "animation: spec 5s forwards;";
-      }
-      else{
-        console.log(isfull())
-        e.target.parentElement.parentElement.style.cssText =
-        "animation: wrap 5s forwards;";
-        }
+    if (
+      isfull() == false &&
+      e.target.parentElement.parentElement.nextSibling === null
+    ) {
+      e.target.parentElement.parentElement.style.cssText =
+        "animation: spec 0.5s forwards;";
+    } else {
+      console.log(isfull());
+      e.target.parentElement.parentElement.style.cssText =
+        "animation: wrap 0.5s forwards;";
+    }
     // }
 
-    //nextSibling 
-      
+    //nextSibling
 
     setTimeout(function () {
       e.target.parentElement.parentElement.remove();
       sav();
-    }, 5000);
+    }, 500);
   } else if (e.target.tagName === "DIV") {
     if (e.target.classList.contains("ondel")) {
       return;
@@ -142,10 +143,10 @@ function load() {
   tasks.innerHTML = localStorage.getItem("data");
   setInterval(() => {
     console.log(isfull());
-  },1000);
+  }, 1000);
 }
 load();
- function isfull(){
+function isfull() {
   let containerhight = document.querySelector(".container").offsetHeight;
   let elmentheight = tasks.offsetHeight; //
   let taskscount = Array.from(document.querySelectorAll(".task")).length;
@@ -153,11 +154,7 @@ load();
   // console.log("elmentheigh : " + elmentheight);
   // console.log("contaienrhight : " + containerhight);
   // console.log("contaienrhight minus : " + (Number(containerhight)  - (65 + 52 +37.5 + 10)));
-  if(elmentheight >= (Number(containerhight)  - (65 + 52 +37.5 + 10))){
+  if (elmentheight >= Number(containerhight) - (65 + 52 + 37.5 + 10)) {
     return true;
-  }
-  else
-  return false;
-
+  } else return false;
 }
-
