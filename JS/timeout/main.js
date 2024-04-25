9c2c64b55b6c925acbbe09fefbec6ff9822aa571
3akrot <105954803+3akrot@users.noreply.github.com>
2024-04-25 15:55:13 +0200

    new

---
+++ JS/timeout/main.js
@@ -0,0 +1,39 @@
+const datepicker = document.getElementById("datepick");
+let days = document.querySelector(".days")
+let hours = document.querySelector(".hours")
+let minutes = document.querySelector(".minutes")
+let seconds = document.querySelector(".sec")
+datepicker.value = new Date();
+// days.appendChild(document.createTextNode("kosomel javscript"))
+datepicker.onchange = function() {
+  localStorage.setItem("currentdate", datepicker.value)
+}
+window.onload = function() {
+  datepicker.value = localStorage.getItem("currentdate");
+}
+
+  setInterval(()=> {
+    if(datepicker.value === "") return
+  comingevent = new Date(datepicker.value);
+  currentatimg = new Date();
+  reamingin = comingevent - currentatimg;
+  if (reamingin < 0) {
+    days.textContent = "0" ;
+    hours.textContent = "0" ;
+    minutes.textContent = "0";
+    seconds.textContent = "0";
+    return 
+  }
+  dayscount =  (Math.floor(reamingin / (1000 * 60 * 60 * 24)))
+  hourscount = (Math.floor(reamingin % (1000 * 60 * 60 * 24) / 1000 / 60 / 60))
+  minutescount = (Math.ceil(reamingin / 1000 / 60 % 60))
+  secondscount = (Math.ceil(reamingin / 1000 % 60))
+  days.textContent =  String(dayscount);
+  hours.textContent =  String(hourscount);
+  minutes.textContent = String(minutescount);
+  seconds.textContent = String(secondscount);
+  },1);
+  
+
+
+
